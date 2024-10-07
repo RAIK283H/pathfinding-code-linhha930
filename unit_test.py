d5235b92d5ec4c24b5b481b59fab2bc8f33422fb
linhha930 <143023730+linhha930@users.noreply.github.com>
2024-10-06 19:23:51 -0500

    Added unit tests (no extra credit)

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,6 @@
 import math
 import unittest
-
+from pathing import generate_random_path 
 
 class TestPathFinding(unittest.TestCase):
 
@@ -26,3 +26,38 @@ class TestPathFinding(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+def setUp(self):
+        # Test graph
+        self.graph = [
+            [(0, 0), [1, 2]],    
+            [(1, 1), [0, 3]],    
+            [(2, 2), [0, 3, 4]], 
+            [(3, 3), [1, 2]],   
+            [(4, 4), [2]],       
+        ]
+    
+def test_generate_random_path(self):
+        start = 0
+        end = 4
+        
+        path = generate_random_path(self.graph, start, end)
+        self.assertTrue(path)
+        self.assertEqual(path[0], start)
+        self.assertEqual(path[-1], end)
+        self.assertIn(end, path)
+        
+        for node in path:
+            self.assertIn(node, range(len(self.graph)))
+
+def test_edge_case_no_path(self):
+        disconnected_graph = [
+            [(0, 0), [1]],
+            [(1, 1), [0]],
+            [(2, 2), []], 
+        ]
+        start = 0
+        end = 2
+        path = generate_random_path(disconnected_graph, start, end)
+        
+        self.assertEqual(path, [])
